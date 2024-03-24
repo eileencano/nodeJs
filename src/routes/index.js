4bbd65e226a69ceb476ccb3d1740d146d69bfa33
eileencano <127575821+eileencano@users.noreply.github.com>
2024-03-24 14:26:30 -0500

    Add files via upload

---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+import {Router} from "express"
+const  router = Router()
+
+//Estaremos modificando el titulo de la pagina
+router.get('/',(req,res)=> res.render('index',{title: 'Bienvenido- Home'}))
+router.get('/about',(req,res)=> res.render('about',{title: 'About '}))
+router.get('/contact',(req,res)=> res.render('contact',{title: 'Contact'}))
+
+
+export default router
